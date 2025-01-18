bc2897c8db6758acf079cb10ec04e57fbcb623c4
evank <infectious@duck.com>
2025-01-18 16:48:51 +0800

    auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css'
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import { Metadata } from 'next'; 
 
+export const metadata: Metadata = {
+  title: 'Home',
+};
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
